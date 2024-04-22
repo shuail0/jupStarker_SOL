fddc745e89d109492ea0ac7422bb5d3dc085821b
shuail0 <11707571@qq.com>
2024-04-22 09:49:15 +0800

    优化代码

--- index.js
+++ index.js
@@ -55,8 +55,8 @@ async function handleMintTask() {
     // 查询SOL余额
     const SOLBalance = await connection.getBalance(wallet.publicKey);
     const jupBalanceInfo = await getSPLBalance(connection, wallet.publicKey, tokenOut);
-    if (SOLBalance < 0.0003 * 10 ** 9 | jupBalanceInfo.uiAmount < 1000 * 10 ** 6) {
-      logger.error(`钱包:${wt.Address}余额不足, SOL余额:${SOLBalance}, JUP余额:${jupBalanceInfo.uiAmount}`);
+    if (SOLBalance < 0.0003 * 10 ** 9 ) {
+      logger.error(`钱包:${wt.Address}余额不足, SOL余额:${SOLBalance}, JUP余额:${jupBalanceInfo.amount}`);
       date = new Date().toLocaleString();
       await appendObjectToCSV({ date, ...wt, Error: '余额不足' }, './logs/StakeError.csv')
       break;
@@ -68,8 +68,18 @@ async function handleMintTask() {
 
 
     while (num < MAX_RETRY) {
+      const currentJupBalanceInfo = await getSPLBalance(connection, wallet.publicKey, tokenOut);
+      if (currentJupBalanceInfo.amount !== jupBalanceInfo.amount) {
+        logger.info(`钱包:${wt.Address}余额发生变化, SOL余额:${SOLBalance}, 初始JUP余额:${jupBalanceInfo.amount}, 当前JUP余额:${currentJupBalanceInfo.amount}`);
+        if (currentJupBalanceInfo.amount < jupBalanceInfo.amount) {
+          logger.success(`当前jup余额小于初始余额，质押成功`);
+          // 获取当前本地时间
+          date = new Date().toLocaleString();
+          await appendObjectToCSV({ date, ...wt }, './logs/StakeSucess.csv')
+          break;
+        }
+      }
       try {
-
         const txid = await p.stake(jupBalanceInfo.amount);
         if (txid) {
           logger.success(`交易成功:https://solscan.io/tx/${txid}`);
@@ -79,8 +89,8 @@ async function handleMintTask() {
           break;
         } else {
           num++;
-          logger.warn('交易失败,休息1秒后重试...');
-          await sleep(0.02);
+          logger.warn('交易失败,休息6秒后重试...');
+          await sleep(0.1);
           if (num === MAX_RETRY) {
             logger.error('重试次数已达上限');
             date = new Date().toLocaleString();
@@ -90,8 +100,8 @@ async function handleMintTask() {
         }
       } catch (error) {
         num++;
-        logger.error(`交易失败,休息1秒后重试...错误原因: ${error}`);
-        await sleep(0.02);
+        logger.error(`交易失败,休息6秒后重试...错误原因: ${error}`);
+        await sleep(0.1);
         if (num === MAX_RETRY) {
           logger.error('重试次数已达上限');
           date = new Date().toLocaleString();
